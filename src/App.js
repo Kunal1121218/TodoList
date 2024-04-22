d5b51917a834e08425af93b535c96a5859ea8969
Kunal Jadhav <112310504+Kunal1121218@users.noreply.github.com>
2024-04-22 18:49:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,41 +4,41 @@ import React, {useEffect, useState} from 'react';
 import Header from "./MyComponents/Header";
 import AddTodo from './MyComponents/AddTodo';
 
-function App() {
-  let initTodo;
-  if(localStorage.getItem("todos")===null){
-    initTodo =[];
-  }
-  else{
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
+// function App() {
+//   let initTodo;
+//   if(localStorage.getItem("todos")===null){
+//     initTodo =[];
+//   }
+//   else{
+//     initTodo = JSON.parse(localStorage.getItem("todos"));
+//   }
   
-  const onDelete =(todo)=>{
-    setTodos(todos.filter((e)=>{
-      return e!== todo;
-    }))
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
-  const addTodo = (title, desc) =>{
-    const myTodo= {
-      title : title,
-      desc : desc
-    }
-    setTodos([...todos, myTodo]);
-  }
+//   const onDelete =(todo)=>{
+//     setTodos(todos.filter((e)=>{
+//       return e!== todo;
+//     }))
+//     localStorage.setItem("todos", JSON.stringify(todos))
+//   }
+//   const addTodo = (title, desc) =>{
+//     const myTodo= {
+//       title : title,
+//       desc : desc
+//     }
+//     setTodos([...todos, myTodo]);
+//   }
 
-  const [todos, setTodos]=useState(initTodo);
-  useEffect(()=>{
-    localStorage.setItem("todos", JSON.stringify(todos))
-  },[todos]);
+//   const [todos, setTodos]=useState(initTodo);
+//   useEffect(()=>{
+//     localStorage.setItem("todos", JSON.stringify(todos))
+//   },[todos]);
 
-  return (
-    <div className="App">
-      <Header/>
-      <AddTodo addTodo={addTodo}/>
-      <Todos todos = {todos} onDelete={onDelete}/>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <AddTodo addTodo={addTodo}/>
+//       <Todos todos = {todos} onDelete={onDelete}/>
+//     </div>
+//   );
+// }
 
 export default App;
